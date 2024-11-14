683bf13dfc21edd5f36ea93dc138b5d360556b1f
daniel <daniel.juarez@ulv.edu.mx>
2024-11-14 08:04:40 -0600

    se agrega endpoint de categorias y productos de categorias

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,7 +1,9 @@
 import {Router}  from 'express'
 import { 
     getPriceProduct,
-    getProducts
+    getProducts,
+    getCategories,
+    getProductsCategorie
 } from '../controllers/products.controllers.js'
 
 const router = Router();
@@ -9,6 +11,8 @@ const router = Router();
 //Crear un modulo para los usuarios
 router.get('/priceproduct/:id/:user', getPriceProduct)
 router.get('/products/:user', getProducts)
+router.get('/categories', getCategories)
+router.get('/productsCategorie/:id/:user', getProductsCategorie)
 
 
 export default router;
