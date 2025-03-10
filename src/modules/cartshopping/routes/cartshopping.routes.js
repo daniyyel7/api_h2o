3ebafed5e09539d439844933b1c8a34514b38d81
sheilarodrj <sheila.rodriguez@ulv.edu.com>
2025-03-09 20:55:36 -0600

    Modificación de los endpoints en routes

--- src/modules/cartshopping/routes/cartshopping.routes.js
+++ src/modules/cartshopping/routes/cartshopping.routes.js
@@ -7,8 +7,8 @@ import {
 
 const router = express.Router();
 
-router.post("/cart", addProductController);
-router.put("/cart", updateProductController);
-router.get("/cart/:userId", getCartProductsController);
+router.post("/cart/add", addProductController);
+router.put("/cart/updateProduct/:id", updateProductController);
+router.get("/cart//products/:id", getCartProductsController);
 
 export default router;
