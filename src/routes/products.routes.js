782c02e30b123ffaab45083e2d9304d96b79b61f
Set Hermor <set.hernandez@ulv.edu.mx>
2025-03-09 23:29:16 -0600

    Routes actualizado

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,7 @@
 import {Router}  from 'express'
-import express from 'express';
 import * as productController from '../controllers/products.controllers.js';
 
-
+ 
 //const router = express.Router();
 // import { 
 //     getPriceProduct,
